09f82d3b4109d9e7c5da08c00874074356160428
Yamato S <ysasaki1218.donau@gmail.com>
2022-01-30 14:37:19 +0900

    move process to controller

--- function.go
+++ function.go
@@ -1,38 +1,16 @@
 package function
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/calendar-open/outbounds"
 	"github.com/calendar-open/usecase"
+	"github.com/calendar-open/web_controller"
 )
 
 func OpenCalendar(w http.ResponseWriter, r *http.Request) {
 	calendarClient := outbounds.NewCalendarClient()
 	usecase := usecase.NewCalendarBatchUsecase(calendarClient)
-
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-	}
-
-	err := usecase.OpenReservationFrame()
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Internal error : %v", err), http.StatusInternalServerError)
-	}
-
-	res := struct {
-		Success bool `json:"success"`
-	}{
-		Success: true,
-	}
-
-	jsonRes, err := json.Marshal(res)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Internal error : %v", err), http.StatusInternalServerError)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonRes)
+	controller := web_controller.NewOpenReservationFrameWebController(usecase)
+	controller.OpenReservationFrame(w, r)
 }
