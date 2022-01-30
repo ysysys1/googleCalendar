d6228a6413ff03abf7c4072b6478f95e06e747b0
Yamato S <ysasaki1218.donau@gmail.com>
2022-01-30 14:19:14 +0900

    add calednar function

--- function.go
+++ function.go
@@ -1,18 +1,38 @@
 package function
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
-	"github.com/calendar-open/hello"
+	"github.com/calendar-open/outbounds"
+	"github.com/calendar-open/usecase"
 )
 
-func Hello(w http.ResponseWriter, r *http.Request) {
+func OpenCalendar(w http.ResponseWriter, r *http.Request) {
+	calendarClient := outbounds.NewCalendarClient()
+	usecase := usecase.NewCalendarBatchUsecase(calendarClient)
 
-	switch r.Method {
-	case http.MethodGet:
-
-		hello.HelloWorld(w, r)
-	default:
+	if r.Method != http.MethodPost {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 	}
+
+	err := usecase.OpenReservationFrame()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Internal error : %v", err), http.StatusInternalServerError)
+	}
+
+	res := struct {
+		Success bool `json:"success"`
+	}{
+		Success: true,
+	}
+
+	jsonRes, err := json.Marshal(res)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Internal error : %v", err), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonRes)
 }
